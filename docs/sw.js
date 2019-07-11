7924daf06dd2469024403992a0a8094e1a0cfb20
keelii <keeliizhou@gmail.com>
2019-07-11 19:09:12 +0800

    first commit

---
+++ docs/sw.js
@@ -0,0 +1,53 @@
+__CACHE_VER__ = "hash.wtdf.io_v1.0.0"
+__CACHE_URLS__ = [
+    "/",
+    "/index.css",
+    "/index.js",
+    "/manifest.json",
+    "/favicon.ico",
+    "https://code.bdstatic.com/npm/@wtdf/joshua@0.0.12/dist/joshua.min.css",
+    "https://fonts.loli.net/css?family=PT+Serif:400|PT+Serif:400&display=swap",
+    "https://fonts.loli.net/css?family=Source+Serif+Pro:400,600,700&display=swap"
+]
+
+self.addEventListener('install', function(evt) {
+    evt.waitUntil(
+        caches.open(__CACHE_VER__).then((cache) => {
+            console.log('[ServiceWorker] Pre-caching offline page')
+            return cache.addAll(__CACHE_URLS__)
+        })
+    )
+})
+
+self.addEventListener('activate', function(evt) {
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== __CACHE_VER__) {
+                    console.log('[ServiceWorker] Removing old cache', key)
+                    return caches.delete(key)
+                }
+            }))
+        })
+    )
+})
+
+self.addEventListener('fetch', function (event) {
+    console.log('[Fetch]', event.request.url)
+    event.respondWith(
+        caches.match(event.request).then(res => {
+            return res ||
+                fetch(event.request)
+                    .then(responese => {
+                        const responeseClone = responese.clone()
+                        caches.open(__CACHE_VER__).then(cache => {
+                            cache.put(event.request, responeseClone)
+                        })
+                        return responese
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+        })
+    )
+})
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import sha256 from 'crypto-js/sha256';
+import sha512 from 'crypto-js/sha512';
+import sha1 from 'crypto-js/sha1';
+import md5 from 'crypto-js/md5';
+
+const fnMap = {
+    sha256, sha512, sha1, md5,
+    def: md5
+}
+
+let id = (id) => document.getElementById(id)
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    id('source').focus()
+    id('source').onkeyup = (e) => {
+        const source = e.target.innerText
+
+        ;['sha256', 'sha512', 'sha1', 'md5'].forEach(m => {
+            if (id(m)) {
+                id(m).value = fnMap[m](source)
+            }
+        })
+    }
+
+})
